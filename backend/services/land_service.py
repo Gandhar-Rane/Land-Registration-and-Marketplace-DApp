c5507eb271747ef22d0ac326f1c48b1263ec0991
Gandhar-Rane <gandhar885@gmail.com>
2026-06-16 16:56:36 +0530

    Removed sensitive configuration

--- backend/services/land_service.py
+++ backend/services/land_service.py
@@ -5,11 +5,11 @@ from werkzeug.utils import secure_filename
 from web3 import Web3
 
 
-# 🔗 CONNECT TO HARDHAT
+
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
-# 🔐 ADMIN PRIVATE KEY (PASTE YOUR HARDHAT KEY HERE)
-PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+PRIVATE_KEY = "Your_Private_Key_Here"  
 ACCOUNT = w3.eth.account.from_key(PRIVATE_KEY)
 
 
